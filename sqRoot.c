227b7ad7757cb090878cfa2384a3ae29f763c05d
MehdiTaher07 <69479765+MehdiTaher07@users.noreply.github.com>
2020-09-28 18:21:51 +0100

    Create sqRoot.c

---
+++ sqRoot.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{   int number;
+    float i;
+    printf("Enter a number to find sq root : ");
+    scanf("%d",&number);
+    for(i=0.01;i*i<number;i+=0.01);
+        printf("%.2f",i);
+    printf("\n%f",sqrt(number));
+        
+    return 0;
+}
+//
+int main()
+{
+    int number;
+    int i;
+    int halfOfNumber;
+    printf("Enter a number to find sq root : ");
+    scanf("%d",&number);
+    halfOfNumber=number/2;
+    for(i=1;i<=halfOfNumber;i++){
+        if(i*i==number){
+            printf("Square root of %d is : %d ",number,i);
+            break;
+        }
+    }
+    if(i>halfOfNumber){
+        printf("%d is not Perfect Square ",number);
+    }
+    return 0;
+}
